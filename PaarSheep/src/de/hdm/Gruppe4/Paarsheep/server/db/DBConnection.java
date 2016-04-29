541fb7a480ed24a01c945185b6bed63c79ffa8d6
tinohauler <th098@hdm-stuttgart.de>
2016-04-29 16:01:42 +0200

    NutzerprofilMapper erstellt

--- PaarSheep/src/de/hdm/Gruppe4/Paarsheep/server/db/DBConnection.java
+++ PaarSheep/src/de/hdm/Gruppe4/Paarsheep/server/db/DBConnection.java
@@ -1,7 +1,61 @@
 package de.hdm.Gruppe4.Paarsheep.server.db;
 
-public class DBConnection { 
-	
-	//test
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 
-}
+import com.google.appengine.api.utils.SystemProperty;
+
+public class DBConnection { 
+	 private static Connection con = null;
+	    private static String googleUrl = "";
+	    private static String localUrl = "jdbc:mysql://127.0.0.1:3306/Paarsheep";
+
+
+	    public static Connection connection() {
+	        
+	        if (con == null) {
+	            String url = null;
+	            try {
+	                if (SystemProperty.environment.value() == SystemProperty.Environment.Value.Production) {
+	                   
+	                    Class.forName("com.mysql.jdbc.GoogleDriver");
+	                    url = googleUrl;
+	                } else {
+	                    
+	                    Class.forName("com.mysql.jdbc.Driver");
+	                    url = localUrl;
+	                }
+
+	                con = DriverManager.getConnection(url);
+	            } catch (Exception e) {
+	                con = null;
+	                e.printStackTrace();
+	            }
+	        }
+
+	        // Zurückgegeben der Verbindung
+	        return con;
+	    }
+	    	   
+	    	    public void getPartner(){
+	    	        if (con != null){
+	    	            Statement query;
+	    	            try {
+	    	                query= con.createStatement();
+	    	                final String sql= "SELECT VORNAME, NACHNAME " + "FROM NUTZERPROFIL ";
+	    	                final ResultSet result = query.executeQuery(sql);
+	    	                while(result.next()){
+	    	                    final String first_name = result.getString("VORNAME");
+	    	                    final String last_name = result.getString("NACHNAME");
+	    	                    final String name = last_name + ", " + first_name;
+	    	                    System.out.println(name);
+	    	                }
+	    	            } catch (final SQLException e) {
+	    	                e.printStackTrace();
+	    	            }
+	    	        }
+	    	    }
+	    	}
