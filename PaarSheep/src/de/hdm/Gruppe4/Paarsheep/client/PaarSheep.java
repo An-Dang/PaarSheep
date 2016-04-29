14ed8bb05d728bc5de4d19c2c7a8d73c0de3eec7
Maurer <fm054@hdm-stuttgart.de>
2016-04-29 10:24:32 +0200

    Einfügen einer login-Klasse

--- PaarSheep/src/de/hdm/Gruppe4/Paarsheep/client/PaarSheep.java
+++ PaarSheep/src/de/hdm/Gruppe4/Paarsheep/client/PaarSheep.java
@@ -1,6 +1,8 @@
 package de.hdm.Gruppe4.Paarsheep.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
 
 
 /**
@@ -12,6 +14,11 @@ public class PaarSheep implements EntryPoint {
 	 * This is the entry point method.
 	 */
 	public void onModuleLoad() {
+		VerticalPanel topPanel = new VerticalPanel();
+		RootPanel.get("TopPAnel").add(topPanel);
+		
+		VerticalPanel botPanel = new VerticalPanel();
+		RootPanel.get("BotPanel").add(botPanel);
 	
 	}
 }
