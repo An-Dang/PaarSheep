2243a7db30d6b241cc4e2a244785b3e6c0ae2252
An Dang <andang@hotmail.de>
2016-05-22 19:26:52 +0200

    Merkzettelform

--- PaarSheep/src/de/hdm/Gruppe4/Paarsheep/client/gui/MerkzettelForm.java
+++ PaarSheep/src/de/hdm/Gruppe4/Paarsheep/client/gui/MerkzettelForm.java
@@ -1,5 +1,15 @@
 package de.hdm.Gruppe4.Paarsheep.client.gui;
 
-public class MerkzettelForm {
+import com.google.gwt.event.dom.client.*;
+import com.google.gwt.user.client.ui.*;
+
+/**
+* Formular für die Darstellung der zu bearbeitenden Merkzettel
+* 
+* @author An Dang
+*/
+
+public class MerkzettelForm extends VerticalPanel {
+
 
 }
