6428eea17d51f420c6f62444df9d308e6c905e5b
An Dang <andang@hotmail.de>
2016-06-29 18:13:36 +0200

    Neue Klassen Hinzugefügt

---
+++ PaarSheep/src/de/hdm/Gruppe4/Paarsheep/client/AnzeigenPartnervorschleageNPReport.java
@@ -0,0 +1,81 @@
+package de.hdm.Gruppe4.Paarsheep.client;
+
+import com.google.gwt.user.client.rpc.AsyncCallback;
+import com.google.gwt.user.client.ui.HTML;
+import com.google.gwt.user.client.ui.Label;
+import com.google.gwt.user.client.ui.RootPanel;
+import com.google.gwt.user.client.ui.VerticalPanel;
+import de.hdm.Gruppe4.Paarsheep.shared.bo.Nutzerprofil;
+import de.hdm.Gruppe4.Paarsheep.shared.report.AllPartnervorschlaegeNpReport;
+import de.hdm.Gruppe4.Paarsheep.shared.report.HTMLReportWriter;
+
+
+/**
+ * @author andang
+ *
+ */
+public class AnzeigenPartnervorschleageNPReport extends VerticalPanel{
+
+	/**
+	 * Neues Nutzerprofil-Objekt anlegen mit Login-Infos.
+	 */
+	private Nutzerprofil nutzerprofil = ClientsideSettings.getAktuellerUser();
+
+	/**
+	 * VerticalPanel hinzufügen.
+	 */
+	private VerticalPanel verPanel = new VerticalPanel();
+
+	/**
+	 * Label zur Information hinzufügen.
+	 */
+	private Label infoLabel = new Label();
+	private Label ueberschriftLabel = new Label();
+	
+	/**
+	 * 
+	 */
+	public AnzeigenPartnervorschleageNPReport(){
+		run();
+	}
+	
+	/**
+	 * 
+	 */
+	public void run(){
+		this.add(verPanel);
+
+		ueberschriftLabel.setText("Ingo Stinkt!#");
+		ueberschriftLabel.addStyleName("partnerboerse-label");
+
+		reportAuslesen();
+		verPanel.add(ueberschriftLabel);
+		verPanel.add(infoLabel);
+	}
+	
+	/**
+	 * Report auslesen.
+	 */
+	
+	public void reportAuslesen() {
+		ClientsideSettings.getReportGenerator().createAllPartnervorschlaegeNpReport(nutzerprofil,
+				new AsyncCallback<AllPartnervorschlaegeNpReport>() {
+			
+					public void onFailure(Throwable caught) {
+						infoLabel.setText("Es trat ein Fehler auf.");
+					}
+					
+					public void onSuccess(AllPartnervorschlaegeNpReport report) {
+						if (report != null) {
+							/*
+							 * Neue HTML-Seite fuer den Report erzeugen.
+							 */
+							HTMLReportWriter writer = new HTMLReportWriter();
+							writer.process(report);
+							RootPanel.get("Container").clear();
+							RootPanel.get("Container").add(new HTML(writer.getReportText()));
+						}
+					}
+				});
+	}
+}
